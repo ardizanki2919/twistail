abbd5ff79098c8286a988806b6c337cb7af9732f
Aris Ripandi <riipandi@gmail.com>
2025-03-09 03:25:59 +0700

    fix(website): opengraph and twitter meta

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -5,11 +5,13 @@ import '../styles/global.css'
 import { RootProvider } from 'fumadocs-ui/provider'
 import type { Metadata } from 'next'
 import { JetBrains_Mono, Mona_Sans } from 'next/font/google'
+import { env } from 'std-env'
 import { cn } from 'twistail-utils'
 
 const fontSans = Mona_Sans({ variable: '--font-sans', subsets: ['latin'] })
 const fontMono = JetBrains_Mono({ variable: '--font-mono', subsets: ['latin'] })
 
+const baseUrl = env.NEXT_PUBLIC_BASE_URL || 'https://twistail.com'
 const metaDescription = `Modular and extensible UI components library powered by Radix UI, Tailwind CSS, and TypeScript.`
 const metaTitle = 'Twistail — React UI components library'
 const ogImageUrl = 'https://twistail.com/images/banner.png'
@@ -19,35 +21,28 @@ export const metadata: Metadata = {
   description: metaDescription,
   keywords: `Open Source,Responsive,UI Components,React,Tailwind CSS v4,Radix UI,TypeScript,Next.js,Server Components`,
   creator: '@riipandi',
+  openGraph: {
+    url: baseUrl,
+    title: metaTitle,
+    description: metaDescription,
+    images: { url: ogImageUrl },
+    siteName: 'Twistail',
+    type: 'website',
+  },
+  twitter: {
+    site: baseUrl,
+    title: metaTitle,
+    description: metaDescription,
+    images: { url: ogImageUrl, alt: metaTitle },
+    card: 'summary_large_image',
+    creator: '@riipandi',
+  },
 }
 
 export default function RootLayout({ children }: Readonly<React.PropsWithChildren>) {
   return (
     <html lang="en" data-theme="light" suppressHydrationWarning>
-      <head>
-        {/* <!-- Open Graph Meta Tags --> */}
-        <meta property="og:url" content="https://twistail.com" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={metaTitle} />
-        <meta property="og:description" content={metaDescription} />
-        <meta property="og:image" content={ogImageUrl} />
-
-        {/* <!-- Twitter Meta Tags --> */}
-        <meta name="twitter:creator" content="@riipandi" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="twistail.com" />
-        <meta property="twitter:url" content="https://twistail.com" />
-        <meta name="twitter:title" content={metaTitle} />
-        <meta name="twitter:description" content={metaDescription} />
-        <meta name="twitter:image" content={ogImageUrl} />
-      </head>
-      <body
-        className={cn(
-          fontSans.variable,
-          fontMono.variable,
-          'font-sans antialiased dark:bg-gray-900'
-        )}
-      >
+      <body className={cn(fontSans.variable, fontMono.variable)}>
         <RootProvider search={{ enabled: false, options: { type: 'static' } }}>
           {children}
         </RootProvider>
