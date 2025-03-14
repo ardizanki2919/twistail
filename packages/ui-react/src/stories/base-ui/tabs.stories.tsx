67025905bd110aa2395077ce6f9bff94fc32fcb6
Aris Ripandi <riipandi@gmail.com>
2025-03-15 00:06:43 +0700

    feat(ui): tabs styles

--- packages/ui-react/src/stories/base-ui/tabs.stories.tsx
+++ packages/ui-react/src/stories/base-ui/tabs.stories.tsx
@@ -5,7 +5,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '#/components'
 const meta: Meta<typeof Tabs> = {
   component: Tabs,
   title: 'Base Components/Tabs',
-  tags: ['status:wip'],
+  tags: ['status:preview'],
   parameters: {},
 }
 
@@ -100,6 +100,104 @@ export const VariantSolid: Story = {
   ),
 }
 
+export const VariantCurved: Story = {
+  render: () => (
+    <Tabs defaultValue="tab1">
+      <TabsList variant="curved">
+        <TabsTrigger value="tab1">Returns</TabsTrigger>
+        <TabsTrigger value="tab2">Shipping</TabsTrigger>
+      </TabsList>
+      <div className="mt-4 ml-2">
+        <TabsContent
+          value="tab1"
+          className="space-y-2 text-gray-600 text-sm leading-7 dark:text-gray-500"
+        >
+          <p>
+            You have 60 days from the time we've shipped your order to return any part of it to us
+            for a refund, provided it is still in its original, unused condition: we do not accept
+            returns of used items.
+          </p>
+          <p>
+            No return authorization (RMA) is required. If you are within the United States, a
+            pre-paid shipping label will be generated. For direct returns, a flat fee of $10 is
+            deducted from your return for shipping and processing costs.
+          </p>
+        </TabsContent>
+        <TabsContent
+          value="tab2"
+          className="space-y-2 text-gray-600 text-sm leading-7 dark:text-gray-500"
+        >
+          <p>
+            We ship worldwide via UPS Expedited. We offer flat rate shipping to customers in Canada
+            ($30), the EU, Japan, and Singapore ($45–$65+), and Australia ($65). Note that most
+            brokerage fees are included in the price of UPS Expedited shipping, with the exception
+            of a possible $10 fee assessed in Canada only if prior arrangements to pay for duties
+            and taxes are not made (see next question and answer).
+          </p>
+          <p>
+            Outside of the United States, tariffs, duties, and taxes are the responsibility of the
+            customer and are usually paid at time of delivery.
+          </p>
+        </TabsContent>
+      </div>
+    </Tabs>
+  ),
+}
+
+export const VariantCurvedWithIcon: Story = {
+  render: () => (
+    <Tabs defaultValue="tab1">
+      <TabsList variant="curved">
+        <TabsTrigger value="tab1" className="gap-1.5">
+          <Lucide.MapPin className="-ml-1 size-4" aria-hidden="true" />
+          Location
+        </TabsTrigger>
+        <TabsTrigger value="tab2" className="gap-1.5">
+          <Lucide.Calculator className="-ml-1 size-4" aria-hidden="true" />
+          Calculation
+        </TabsTrigger>
+        <TabsTrigger value="tab3" className="gap-1.5" disabled>
+          <Lucide.Book className="-ml-1 size-4" aria-hidden="true" />
+          User Guide
+        </TabsTrigger>
+      </TabsList>
+      <div className="mt-4 ml-2">
+        <TabsContent
+          value="tab1"
+          className="space-y-2 text-gray-600 text-sm leading-7 dark:text-gray-500"
+        >
+          <p>
+            You have 60 days from the time we've shipped your order to return any part of it to us
+            for a refund, provided it is still in its original, unused condition: we do not accept
+            returns of used items.
+          </p>
+          <p>
+            No return authorization (RMA) is required. If you are within the United States, a
+            pre-paid shipping label will be generated. For direct returns, a flat fee of $10 is
+            deducted from your return for shipping and processing costs.
+          </p>
+        </TabsContent>
+        <TabsContent
+          value="tab2"
+          className="space-y-2 text-gray-600 text-sm leading-7 dark:text-gray-500"
+        >
+          <p>
+            We ship worldwide via UPS Expedited. We offer flat rate shipping to customers in Canada
+            ($30), the EU, Japan, and Singapore ($45–$65+), and Australia ($65). Note that most
+            brokerage fees are included in the price of UPS Expedited shipping, with the exception
+            of a possible $10 fee assessed in Canada only if prior arrangements to pay for duties
+            and taxes are not made (see next question and answer).
+          </p>
+          <p>
+            Outside of the United States, tariffs, duties, and taxes are the responsibility of the
+            customer and are usually paid at time of delivery.
+          </p>
+        </TabsContent>
+      </div>
+    </Tabs>
+  ),
+}
+
 export const VariantLineWithIcons: Story = {
   render: () => (
     <Tabs defaultValue="tab1">
@@ -138,11 +236,10 @@ export const VariantSolidWithIcons: Story = {
           Calculation
         </TabsTrigger>
       </TabsList>
-
-      <TabsContent value="tab1">
+      <TabsContent value="tab1" className="p-2">
         <p className="text-gray-500 text-sm sm:text-gray-500">Tab 1 content</p>
       </TabsContent>
-      <TabsContent value="tab2">
+      <TabsContent value="tab2" className="p-2">
         <p className="text-gray-500 text-sm sm:text-gray-500">Tab 2 content</p>
       </TabsContent>
     </Tabs>
