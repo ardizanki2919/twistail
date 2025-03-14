5e9e0eac173ccd9c8a2d2b9d0495819410cc8708
Aris Ripandi <riipandi@gmail.com>
2025-03-14 22:45:52 +0700

    feat(ui): input styles

--- packages/ui-react/src/components/base-ui/input/input.tsx
+++ packages/ui-react/src/components/base-ui/input/input.tsx
@@ -2,7 +2,6 @@
 
 import * as Lucide from 'lucide-react'
 import * as React from 'react'
-import { clx } from 'twistail-utils'
 import { type InputStyles, inputStyles } from './input.css'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>, InputStyles {
@@ -15,66 +14,38 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     forwardedRef
   ) => {
     const [typeState, setTypeState] = React.useState(type)
-
     const isPassword = type === 'password'
     const isSearch = type === 'search'
+    const styles = inputStyles({ isSearch, isPassword, hasError, enableStepper })
 
     return (
-      <div className={clx('relative w-full', className)}>
+      <div className={styles.container({ className })}>
         <input
           ref={forwardedRef}
           type={isPassword ? typeState : type}
-          className={clx(
-            inputStyles({ hasError, enableStepper }),
-            {
-              'pl-8': isSearch,
-              'pr-10': isPassword,
-            },
-            inputClassName
-          )}
+          className={styles.input({ className: inputClassName })}
           {...props}
         />
         {isSearch && (
-          <div
-            className={clx(
-              // base
-              'pointer-events-none absolute bottom-0 left-2 flex h-full items-center justify-center',
-              // text color
-              'text-gray-400 dark:text-gray-600'
-            )}
-          >
-            <Lucide.Search className="size-[1.125rem] shrink-0" aria-hidden="true" />
+          <div className={styles.leftIconContainer()}>
+            <Lucide.Search className={styles.leftIcon()} aria-hidden="true" />
           </div>
         )}
         {isPassword && (
-          <div
-            className={clx(
-              'absolute right-0 bottom-0 flex h-full items-center justify-center px-3'
-            )}
-          >
+          <div className={styles.passwordContainer()}>
             <button
-              aria-label="Change password visibility"
-              className={clx(
-                // base
-                'h-fit w-fit rounded-xs outline-hidden transition-all',
-                // text
-                'text-gray-400 dark:text-gray-600',
-                // hover
-                'hover:text-gray-500 hover:dark:text-gray-500',
-                'outline-0 outline-blue-500 outline-offset-2 focus-visible:outline-2 dark:outline-blue-500' /* focusRing */
-              )}
               type="button"
-              onClick={() => {
-                setTypeState(typeState === 'password' ? 'text' : 'password')
-              }}
+              aria-label="Change password visibility"
+              className={styles.passwordButton()}
+              onClick={() => setTypeState(typeState === 'password' ? 'text' : 'password')}
             >
               <span className="sr-only">
                 {typeState === 'password' ? 'Show password' : 'Hide password'}
               </span>
               {typeState === 'password' ? (
-                <Lucide.Eye aria-hidden="true" className="size-5 shrink-0" />
+                <Lucide.Eye aria-hidden="true" className={styles.passwordIcon()} />
               ) : (
-                <Lucide.EyeOff aria-hidden="true" className="size-5 shrink-0" />
+                <Lucide.EyeOff aria-hidden="true" className={styles.passwordIcon()} />
               )}
             </button>
           </div>
--- packages/ui-react/src/stories/base-ui/checkbox.stories.tsx
+++ packages/ui-react/src/stories/base-ui/checkbox.stories.tsx
@@ -5,7 +5,7 @@ import { Button, Checkbox, Label } from '#/components'
 const meta: Meta<typeof Checkbox> = {
   component: Checkbox,
   title: 'Base Components/Checkbox',
-  tags: ['status:wip'],
+  tags: ['status:preview'],
 }
 
 export default meta
