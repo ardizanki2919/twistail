4eee46fc2dd1e4c8e18a724a0c500025cf7bacbc
Aris Ripandi <riipandi@gmail.com>
2025-03-15 21:58:15 +0700

    feat(ui): radio group styles

--- packages/ui-react/src/components/base-ui/radio-group/radio-group.css.ts
+++ packages/ui-react/src/components/base-ui/radio-group/radio-group.css.ts
@@ -1,11 +1,35 @@
 import { type VariantProps, tv } from 'tailwind-variants'
 
 const radioGroupStyles = tv({
-  base: [],
-  slots: {},
-  variants: {},
-  compoundVariants: [],
-  defaultVariants: {},
+  slots: {
+    root: 'grid gap-2',
+    indicator: 'flex items-center justify-center',
+    indicatorInner: [
+      // base
+      'size-1.5 shrink-0 rounded-full',
+      // indicator
+      'bg-white',
+      // disabled
+      'group-data-[disabled]:bg-gray-400 group-data-[disabled]:dark:bg-gray-500',
+    ],
+    item: 'group relative flex size-4 appearance-none items-center justify-center outline-hidden',
+    itemInner: [
+      // base
+      'flex size-4 shrink-0 items-center justify-center rounded-full border shadow-xs',
+      // border color
+      'border-gray-300 dark:border-gray-800',
+      // background color
+      'bg-white dark:bg-gray-950',
+      // checked
+      'group-data-[state=checked]:border-0 group-data-[state=checked]:border-transparent group-data-[state=checked]:bg-blue-500',
+      // disabled
+      'group-data-[disabled]:border',
+      'group-data-[disabled]:border-gray-300 group-data-[disabled]:bg-gray-100 group-data-[disabled]:text-gray-400',
+      'group-data-[disabled]:dark:border-gray-700 group-data-[disabled]:dark:bg-gray-800',
+      // focus
+      'outline-0 outline-blue-500 outline-offset-2 focus-visible:outline-2 dark:outline-blue-500' /* focusRing */,
+    ],
+  },
 })
 
 type RadioGroupStyles = VariantProps<typeof radioGroupStyles>
--- packages/ui-react/src/components/base-ui/radio-group/radio-group.tsx
+++ packages/ui-react/src/components/base-ui/radio-group/radio-group.tsx
@@ -2,86 +2,54 @@
 
 import { RadioGroup as RadioGroupPrimitives } from 'radix-ui'
 import * as React from 'react'
-import { clx } from 'twistail-utils'
+import { radioGroupStyles } from './radio-group.css'
 
 const RadioGroup = React.forwardRef<
   React.ComponentRef<typeof RadioGroupPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitives.Root>
 >(({ className, ...props }, forwardedRef) => {
+  const styles = radioGroupStyles()
   return (
     <RadioGroupPrimitives.Root
       ref={forwardedRef}
-      className={clx('grid gap-2', className)}
+      className={styles.root({ className })}
       {...props}
     />
   )
 })
 
-RadioGroup.displayName = 'RadioGroup'
-
 const RadioGroupIndicator = React.forwardRef<
   React.ComponentRef<typeof RadioGroupPrimitives.Indicator>,
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitives.Indicator>
 >(({ className, ...props }, forwardedRef) => {
+  const styles = radioGroupStyles()
   return (
     <RadioGroupPrimitives.Indicator
       ref={forwardedRef}
-      className={clx('flex items-center justify-center', className)}
+      className={styles.indicator({ className })}
       {...props}
     >
-      <div
-        className={clx(
-          // base
-          'size-1.5 shrink-0 rounded-full',
-          // indicator
-          'bg-white',
-          // disabled
-          'group-data-[disabled]:bg-gray-400 group-data-[disabled]:dark:bg-gray-500'
-        )}
-      />
+      <div className={styles.indicatorInner()} />
     </RadioGroupPrimitives.Indicator>
   )
 })
-
-RadioGroupIndicator.displayName = 'RadioGroupIndicator'
 
 const RadioGroupItem = React.forwardRef<
   React.ComponentRef<typeof RadioGroupPrimitives.Item>,
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitives.Item>
 >(({ className, ...props }, forwardedRef) => {
+  const styles = radioGroupStyles()
   return (
-    <RadioGroupPrimitives.Item
-      ref={forwardedRef}
-      className={clx(
-        'group relative flex size-4 appearance-none items-center justify-center outline-hidden',
-        className
-      )}
-      {...props}
-    >
-      <div
-        className={clx(
-          // base
-          'flex size-4 shrink-0 items-center justify-center rounded-full border shadow-xs',
-          // border color
-          'border-gray-300 dark:border-gray-800',
-          // background color
-          'bg-white dark:bg-gray-950',
-          // checked
-          'group-data-[state=checked]:border-0 group-data-[state=checked]:border-transparent group-data-[state=checked]:bg-blue-500',
-          // disabled
-          'group-data-[disabled]:border',
-          'group-data-[disabled]:border-gray-300 group-data-[disabled]:bg-gray-100 group-data-[disabled]:text-gray-400',
-          'group-data-[disabled]:dark:border-gray-700 group-data-[disabled]:dark:bg-gray-800',
-          // focus
-          'outline-0 outline-blue-500 outline-offset-2 focus-visible:outline-2 dark:outline-blue-500' /* focusRing */
-        )}
-      >
+    <RadioGroupPrimitives.Item ref={forwardedRef} className={styles.item({ className })} {...props}>
+      <div className={styles.itemInner()}>
         <RadioGroupIndicator />
       </div>
     </RadioGroupPrimitives.Item>
   )
 })
 
+RadioGroup.displayName = 'RadioGroup'
+RadioGroupIndicator.displayName = 'RadioGroupIndicator'
 RadioGroupItem.displayName = 'RadioGroupItem'
 
 export { RadioGroup, RadioGroupItem }
