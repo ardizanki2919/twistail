5ef761f2f93f9f262e01f6a94eabaaa6adc8412a
Aris Ripandi <riipandi@gmail.com>
2025-03-16 05:42:50 +0700

    feat(ui): add grouped button storybook

--- packages/ui-react/src/stories/base-ui/button.stories.tsx
+++ packages/ui-react/src/stories/base-ui/button.stories.tsx
@@ -1,12 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { fn } from '@storybook/test'
 import * as Lucide from 'lucide-react'
-import { Button, type ButtonProps, buttonStyles } from '#/components'
+import * as React from 'react'
+import { clx } from 'twistail-utils'
+import { DropdownMenu, DropdownMenuTrigger } from '#/components'
+import { DropdownMenuContent, DropdownMenuItem } from '#/components'
+import { Badge, Button, type ButtonProps, buttonStyles } from '#/components'
 
 const meta: Meta<ButtonProps> = {
   component: Button,
   title: 'Base Components/Button',
   tags: ['status:preview'],
+  parameters: {
+    layout: 'centered',
+  },
   argTypes: {
     children: {
       control: 'text',
@@ -181,32 +188,107 @@ export const AnchorWithVariant: Story = {
   ),
 }
 
-// export const IconShowcase: Story = {
-//   parameters: {
-//     controls: { exclude: ['children', 'asChild', 'variant'] },
-//   },
-//   args: {
-//     size: 'md',
-//     isLoading: false,
-//     disabled: false,
-//   },
-//   render: (args) => (
-//     <div className="flex flex-wrap items-center gap-4">
-//       <Button {...args}>
-//         <Lucide.Search className="-ml-0.5" />
-//         Search
-//       </Button>
-//       <Button {...args} variant="secondary">
-//         <Lucide.Mail className="-ml-0.5" />
-//         Email
-//       </Button>
-//       <Button {...args} variant="outline">
-//         <Lucide.Github className="-ml-0.5" />
-//         Github
-//       </Button>
-//       <Button {...args} size="icon" variant="ghost">
-//         <Lucide.Bell className="-ml-0.5" />
-//       </Button>
-//     </div>
-//   ),
-// }
+export const GroupButtons: Story = {
+  render: () => (
+    <div className="flex flex-wrap gap-4">
+      <div className="-space-x-px isolate flex">
+        <Button variant="outline" className="rounded-r-none focus:z-10">
+          Prev
+        </Button>
+        <Button variant="outline" className="rounded-l-none focus:z-10">
+          Next
+        </Button>
+      </div>
+      <div className="-space-x-px isolate flex">
+        <Button variant="outline" className="rounded-r-none focus:z-10">
+          Prev
+        </Button>
+        <Button variant="outline" className="rounded-none focus:z-10">
+          Now
+        </Button>
+        <Button variant="outline" className="rounded-l-none focus:z-10">
+          Next
+        </Button>
+      </div>
+    </div>
+  ),
+}
+
+export const GroupWithMenu: Story = {
+  render: () => {
+    const [starCount, setStarCount] = React.useState(148)
+    const [isStarred, setIsStarred] = React.useState(false)
+
+    const handleStar = () => {
+      if (!isStarred) {
+        setStarCount(starCount + 1)
+      } else {
+        setStarCount(Math.max(0, starCount - 1))
+      }
+      setIsStarred(!isStarred)
+    }
+
+    return (
+      <div className="flex">
+        <Button
+          onClick={handleStar}
+          variant={isStarred ? 'secondary' : 'outline'}
+          className="flex h-9 items-center gap-2 rounded-r-none border-r-0 px-4"
+        >
+          <Lucide.Star
+            className={clx(isStarred ? 'fill-primary text-primary' : 'fill-none', 'size-4')}
+            strokeWidth={2}
+          />
+          <span>{isStarred ? 'Starred' : 'Star'}</span>
+          <Badge variant={isStarred ? 'primary' : 'neutral'} className="-me-1 font-mono">
+            {starCount}
+          </Badge>
+        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="h-9 rounded-l-none px-2">
+              <Lucide.ChevronDown className="size-4 stroke-2" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Set repository to public</DropdownMenuItem>
+            <DropdownMenuItem>Add to list</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    )
+  },
+}
+
+export const IconShowcase: Story = {
+  parameters: {
+    controls: { exclude: ['children', 'asChild', 'variant'] },
+  },
+  args: {
+    size: 'md',
+    isLoading: false,
+    disabled: false,
+  },
+  render: (args) => (
+    <div className="flex flex-wrap items-center gap-4">
+      <Button {...args}>
+        <Lucide.Search className="-ml-0.5 mr-2 size-4" />
+        <span>Search</span>
+      </Button>
+      <Button {...args} variant="secondary">
+        <Lucide.Mail className="-ml-0.5 mr-2 size-4" />
+        <span>Email</span>
+      </Button>
+      <Button {...args} variant="outline">
+        <Lucide.Github className="-ml-0.5 mr-2 size-4" />
+        <span>Github</span>
+      </Button>
+      <Button {...args} size="icon" variant="outline">
+        <Lucide.MonitorCog className="size-4" />
+      </Button>
+      <Button {...args} size="icon" variant="ghost">
+        <Lucide.Bell className="size-4" />
+      </Button>
+    </div>
+  ),
+}
