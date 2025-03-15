f5434926eddb0c4bb6048b89c2951e63ca6f6340
Aris Ripandi <riipandi@gmail.com>
2025-03-16 02:12:06 +0700

    feat(ui): pagination component

--- packages/ui-react/src/components/base-ui/pagination/pagination.css.ts
+++ packages/ui-react/src/components/base-ui/pagination/pagination.css.ts
@@ -1,13 +1,33 @@
 import { type VariantProps, tv } from 'tailwind-variants'
 
 const paginationStyles = tv({
-  base: [],
-  slots: {},
-  variants: {},
-  compoundVariants: [],
-  defaultVariants: {},
+  slots: {
+    root: 'mx-auto flex w-full justify-center',
+    content: 'flex flex-row items-center',
+    item: [],
+    link: [],
+    previous: 'gap-1 pl-2.5',
+    next: 'gap-1 pr-2.5',
+    previousIcon: 'size-4 -ml-1.5 mr-1',
+    nextIcon: 'size-4 -mr-1.5 ml-1',
+    ellipsis: 'flex h-9 w-9 items-center justify-center',
+    ellipsisIcon: 'size-4',
+  },
+  variants: {
+    variant: {
+      default: {
+        content: 'gap-1',
+      },
+      boxed: {
+        content: 'gap-1 rounded-lg border border-border p-1',
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
 })
 
-type PagnationStyles = VariantProps<typeof paginationStyles>
+type PaginationStyles = VariantProps<typeof paginationStyles>
 
-export { paginationStyles, type PagnationStyles }
+export { paginationStyles, type PaginationStyles }
--- packages/ui-react/src/stories/base-ui/pagination.stories.tsx
+++ packages/ui-react/src/stories/base-ui/pagination.stories.tsx
@@ -1,17 +1,124 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Slot } from 'radix-ui'
+import { Pagination, PaginationContent, PaginationEllipsis } from '#/components'
+import { PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '#/components'
 
-const DummyComponent = Slot.Root
-
-const meta: Meta<typeof DummyComponent> = {
-  component: DummyComponent,
+const meta: Meta<typeof Pagination> = {
+  component: Pagination,
   title: 'Base Components/Pagination',
-  tags: ['status:planned'],
+  tags: ['status:preview'],
+  parameters: {
+    layout: 'centered',
+  },
 }
 
 export default meta
-type Story = StoryObj<typeof DummyComponent>
+type Story = StoryObj<typeof Pagination>
 
 export const Default: Story = {
-  render: () => <DummyComponent />,
+  render: () => (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">8</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">9</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">10</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  ),
+}
+
+export const Boxed: Story = {
+  render: () => (
+    <Pagination>
+      <PaginationContent variant="boxed">
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">8</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">9</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">10</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  ),
+}
+
+export const WithCounter: Story = {
+  render: () => (
+    <Pagination className="flex w-[600px] items-center justify-between">
+      <p className="hidden font-medium text-sm md:block">Showing 2 of 25</p>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">8</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">9</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  ),
 }
