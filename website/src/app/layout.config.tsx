742b0e927ff78d899f570765744f00fac69a86b4
Aris Ripandi <riipandi@gmail.com>
2025-03-16 20:57:58 +0700

    feat(docs): documentation for base components

--- website/src/app/layout.config.tsx
+++ website/src/app/layout.config.tsx
@@ -24,7 +24,7 @@ const TitleComponent = () => (
 )
 
 export const baseOptions: BaseLayoutProps = {
-  nav: { url: '/', title: <TitleComponent />, transparentMode: 'top' },
+  nav: { url: '/', title: <TitleComponent />, transparentMode: 'none' },
   githubUrl: 'https://github.com/riipandi/twistail',
   links: [
     { text: 'Blog', url: '/blog' },
