171564661fff2f6d07067278b9eb63479b857717
Aris Ripandi <riipandi@gmail.com>
2025-03-20 06:15:53 +0700

    refactor: redefine docs structure

--- packages/ui-react/src/components/layout/menubar/menubar.tsx
+++ packages/ui-react/src/components/layout/menubar/menubar.tsx
@@ -69,14 +69,17 @@ const MenubarSubTrigger = React.forwardRef<
 const MenubarSubContent = React.forwardRef<
   React.ComponentRef<typeof MenubarPrimitive.SubContent>,
   React.ComponentPropsWithoutRef<typeof MenubarPrimitive.SubContent>
->(({ className, ...props }, forwardedRef) => {
+>(({ className, sideOffset = 2, ...props }, forwardedRef) => {
   const styles = menubarStyles()
   return (
-    <MenubarPrimitive.SubContent
-      ref={forwardedRef}
-      className={styles.subContent({ className })}
-      {...props}
-    />
+    <MenubarPrimitive.Portal>
+      <MenubarPrimitive.SubContent
+        ref={forwardedRef}
+        className={styles.subContent({ className })}
+        sideOffset={sideOffset}
+        {...props}
+      />
+    </MenubarPrimitive.Portal>
   )
 })
 
@@ -101,18 +104,29 @@ const MenubarContent = React.forwardRef<
 
 const MenubarItem = React.forwardRef<
   React.ComponentRef<typeof MenubarPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Item> & MenubarStyles
->(({ className, inset, ...props }, forwardedRef) => {
+  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Item> &
+    MenubarStyles & {
+      shortcut?: string
+      hint?: string
+    }
+>(({ className, inset, shortcut, hint, children, ...props }, forwardedRef) => {
   const styles = menubarStyles({ inset })
   return (
-    <MenubarPrimitive.Item ref={forwardedRef} className={styles.item({ className })} {...props} />
+    <MenubarPrimitive.Item ref={forwardedRef} className={styles.item({ className })} {...props}>
+      {children}
+      {hint && <span className={styles.itemHint()}>{hint}</span>}
+      {shortcut && <span className={styles.shortcut()}>{shortcut}</span>}
+    </MenubarPrimitive.Item>
   )
 })
 
 const MenubarCheckboxItem = React.forwardRef<
   React.ComponentRef<typeof MenubarPrimitive.CheckboxItem>,
-  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.CheckboxItem>
->(({ className, children, checked, ...props }, forwardedRef) => {
+  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.CheckboxItem> & {
+    shortcut?: string
+    hint?: string
+  }
+>(({ className, children, checked, shortcut, hint, ...props }, forwardedRef) => {
   const styles = menubarStyles()
   return (
     <MenubarPrimitive.CheckboxItem
@@ -121,20 +135,25 @@ const MenubarCheckboxItem = React.forwardRef<
       checked={checked}
       {...props}
     >
-      <span className={styles.checkboxItemInner()}>
+      <span className={styles.checkboxItemIndicator()}>
         <MenubarPrimitive.ItemIndicator>
-          <Lucide.Check className={styles.checkboxItemIndicator()} />
+          <Lucide.Check className={styles.checkboxItemIndicatorIcon()} />
         </MenubarPrimitive.ItemIndicator>
       </span>
       {children}
+      {hint && <span className={styles.checkboxItemHint()}>{hint}</span>}
+      {shortcut && <span className={styles.checkboxItemShortcut()}>{shortcut}</span>}
     </MenubarPrimitive.CheckboxItem>
   )
 })
 
 const MenubarRadioItem = React.forwardRef<
   React.ComponentRef<typeof MenubarPrimitive.RadioItem>,
-  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.RadioItem>
->(({ className, children, ...props }, forwardedRef) => {
+  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.RadioItem> & {
+    shortcut?: string
+    hint?: string
+  }
+>(({ className, children, shortcut, hint, ...props }, forwardedRef) => {
   const styles = menubarStyles()
   return (
     <MenubarPrimitive.RadioItem
@@ -142,12 +161,15 @@ const MenubarRadioItem = React.forwardRef<
       className={styles.radioItem({ className })}
       {...props}
     >
-      <span className={styles.radioItemInner()}>
+      <span className={styles.radioItemIndicator()}>
         <MenubarPrimitive.ItemIndicator>
-          <Lucide.Circle className={styles.radioItemIndicator()} />
+          <Lucide.Check className={styles.radioItemIndicatorIconCheck()} aria-hidden="true" />
+          <Lucide.Circle className={styles.radioItemIndicatorIconCircle()} aria-hidden="true" />
         </MenubarPrimitive.ItemIndicator>
       </span>
       {children}
+      {hint && <span className={styles.radioItemHint()}>{hint}</span>}
+      {shortcut && <span className={styles.radioItemShortcut()}>{shortcut}</span>}
     </MenubarPrimitive.RadioItem>
   )
 })
@@ -176,11 +198,6 @@ const MenubarSeparator = React.forwardRef<
   )
 })
 
-const MenubarShortcut = ({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) => {
-  const styles = menubarStyles()
-  return <span className={styles.shortcut({ className })} {...props} />
-}
-
 Menubar.displayName = MenubarPrimitive.Root.displayName
 MenubarTrigger.displayName = MenubarPrimitive.Trigger.displayName
 MenubarSubTrigger.displayName = MenubarPrimitive.SubTrigger.displayName
@@ -191,7 +208,6 @@ MenubarCheckboxItem.displayName = MenubarPrimitive.CheckboxItem.displayName
 MenubarRadioItem.displayName = MenubarPrimitive.RadioItem.displayName
 MenubarLabel.displayName = MenubarPrimitive.Label.displayName
 MenubarSeparator.displayName = MenubarPrimitive.Separator.displayName
-MenubarShortcut.displayName = 'MenubarShortcut'
 
 export {
   Menubar,
@@ -209,5 +225,4 @@ export {
   MenubarSubTrigger,
   MenubarGroup,
   MenubarSub,
-  MenubarShortcut,
 }
