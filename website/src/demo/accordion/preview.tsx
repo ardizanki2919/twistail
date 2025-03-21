bf93abdc6d5cb0c12af86faa74ac8edb9d5e447e
Aris Ripandi <riipandi@gmail.com>
2025-03-22 04:09:06 +0700

    feat(docs): add accordion demo

--- website/src/demo/accordion/preview.tsx
+++ website/src/demo/accordion/preview.tsx
@@ -7,21 +7,43 @@ import {
 
 export default function AccordionDemo() {
   return (
-    <Accordion type="single" collapsible className="w-full">
+    <Accordion type="single" className="mx-auto mt-3 w-md" collapsible>
       <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionTrigger>In the app</AccordionTrigger>
         <AccordionContent>
-          Yes. It comes with default styles that matches the other components&apos; aesthetic.
+          <ol className="flex flex-col gap-2">
+            <li>
+              <span className="font-semibold text-gray-900 dark:text-gray-50">Step 1:</span> Tap the
+              Inbox icon and then tap Add receipts.
+            </li>
+            <li>
+              <span className="font-semibold text-gray-900 dark:text-gray-50">Step 2:</span> Tap the
+              + symbol to attach a photo or PDF of the receipt for our system to match.
+            </li>
+          </ol>
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Via browser extension</AccordionTrigger>
         <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you prefer.
+          <ol className="flex flex-col gap-2">
+            <li>
+              <span className="font-semibold text-gray-900 dark:text-gray-50">Step 1:</span>{' '}
+              <span className="underline">Download</span> the browser extension for Chrome or
+              Safari. (Firefox support is coming soon.)
+            </li>
+            <li>
+              <span className="font-semibold text-gray-900 dark:text-gray-50">Step 2:</span> Click
+              the extension icon at the top of your browser. Under the Receipts tab, upload an image
+              or click Screenshot current tab to automatically attach the receipt to the expense.
+            </li>
+          </ol>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3" disabled>
+        <AccordionTrigger>Via email forwarding</AccordionTrigger>
+        <AccordionContent>
+          Yes! You can animate the Accordion with CSS or JavaScript.
         </AccordionContent>
       </AccordionItem>
     </Accordion>
