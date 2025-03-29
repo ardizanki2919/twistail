a43f9bb45f815cc80bb9d9627208a23678c4bf5b
Aris Ripandi <riipandi@gmail.com>
2025-03-29 08:10:54 +0700

    feat(ui): add bar list component

--- website/src/demo/bar-list/preview.tsx
+++ website/src/demo/bar-list/preview.tsx
@@ -1,7 +1,24 @@
 'use client'
 
-// import {} from 'twistail-react'
+import { BarList } from 'twistail-react/bar-list'
 
 export default function Demo() {
-  return null
+  const basicData = [
+    { title: '/home', value: 843, maxValue: 1000 },
+    { title: '/imprint', value: 46, maxValue: 1000 },
+    { title: '/cancellation', value: 3, maxValue: 1000 },
+    { title: '/blocks', value: 108, maxValue: 1000 },
+    { title: '/documentation', value: 384, maxValue: 1000 },
+  ]
+
+  const dataWithSubtitles = basicData.map((item) => ({
+    ...item,
+    subtitle: `${Math.round((item.value / item.maxValue) * 100)}% of total traffic`,
+  }))
+
+  return (
+    <div className="mx-auto w-full max-w-lg">
+      <BarList data={dataWithSubtitles} />
+    </div>
+  )
 }
